ed3867ef310261a540f68009715ed7469c18a123
Luma <luuh_lopees@live.com>
2023-09-16 13:16:52 -0300

    ✨ 1h

--- exercicios/exercicio08.js
+++ exercicios/exercicio08.js
@@ -3,6 +3,10 @@ const array = [3, 5, 1, 6, 2, 4, 7, 9, 8, 10];
 
 const arrayMultiplicado = () => {
   // Seu codigo aqui...
+  const mult = array.map((numeros) => `${numeros} * 2 = ${numeros * 2}`)
+  return mult;
 };
+
+console.log(arrayMultiplicado());
 
 module.exports = arrayMultiplicado;
